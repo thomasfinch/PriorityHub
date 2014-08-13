ab77b97998ed6dfc06ae0c3277c2826e1f21cd48
thomasfinch <tomf64@gmail.com>
2014-08-13 00:44:32 -0400

    Removed donation link in settings

--- preferences/PriorityHub.mm
+++ preferences/PriorityHub.mm
@@ -39,12 +39,6 @@
     }
 }
 
--(void)DonateButtonTapped
-{
-    NSURL *donateURL = [NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=tomf64%40gmail%2ecom&lc=US&item_name=Thomas%20Finch&item_number=Priority%20Lock&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHostedGuest"];
-    [[UIApplication sharedApplication] openURL:donateURL];
-}
-
 -(void)GithubButtonTapped
 {
     NSURL *githubURL = [NSURL URLWithString:@"https://github.com/thomasfinch/Priority-Hub"];
